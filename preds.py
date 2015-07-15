6bf854d2d9d0bfc4b31d230ce3480c338f9f8137
Matsumotos <matsumoto@berkeley.edu>
2015-07-15 12:03:17 -0700

    Update preds.py

--- preds.py
+++ preds.py
@@ -7,7 +7,7 @@ word = f.readline().split('\t')
 for line in f:
     new = line.split('\t')
     if new[0] == word[0] and new[3] == word[3] and new[4] == word[4]:
-	word[2] = word[2] + '`' + new[2]
+	word[2] = word[2] + ',' + new[2]
     else:
         f2.write(word[0] + '\t' + word[1] + '\t' + word[2] + '\t' + word[3] + '\t' + word[4])
         word = new
